2b61d47de1c6c5b42c9a8af63eb6089ab17cd0e7
Bluey1562 <rizalakmal@idn.sch.id>
2026-01-22 10:59:52 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from microbit import *
-import utime
 
+# INIT UART
 uart.init(baudrate=115200, tx=pin16, rx=pin15)
 
-def send(cmd, t=800):
+def send(cmd, delay_ms=800):
     uart.write(cmd + "\r\n")
-    sleep(t)
+    sleep(delay_ms)
 
+# TEST ESP
 send("AT")
+
+# CONNECT WIFI
 send('AT+CWJAP="abc","12345678"', 5000)
+
+def http_put(value):
+    send('AT+CIPSTART="TCP","xxx.firebaseio.com",80', 2000)
+
+    payload = str(value)
+    http = (
+        "PUT /temp.json HTTP/1.1\r\n"
+        "Host: xxx.firebaseio.com\r\n"
+        "Content-Length: {}\r\n\r\n{}"
+    ).format(len(payload), payload)
+
+    send('AT+CIPSEND={}'.format(len(http)), 1000)
+    uart.write(http)
+    sleep(2000)
 
 while True:
     temp = temperature()
     http_put(temp)
-    utime.sleep(10)
+    sleep(10000)
--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-let kendaraan = "mobil"
-if (kendaraan == "motor") {
-    basic.showString(" bayar 2000")
-}
-
-if (kendaraan == "mobil") {
-    basic.showString(" bayar 5000")
-}
 
