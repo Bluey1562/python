ef8c11663e0a6e0af28746c82fe6fa115e812120
Bluey1562 <rizalakmal@idn.sch.id>
2026-01-22 10:47:38 +0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+
+kendaraan = "mobil"
+
+if kendaraan == "motor":
+    basic.show_string(" bayar 2000")
+if kendaraan == "mobil":
+    basic.show_string(" bayar 5000")
---
+++ main.ts
@@ -0,0 +1,9 @@
+let kendaraan = "mobil"
+if (kendaraan == "motor") {
+    basic.showString(" bayar 2000")
+}
+
+if (kendaraan == "mobil") {
+    basic.showString(" bayar 5000")
+}
+
